ee43aae777390b281caafd09fe3c014404047391
Siarhei Filinski <sfilisnky@gmail.com>
2022-03-25 12:52:53 +0100

    initial commit

---
+++ packages/backend/src/index.ts
@@ -0,0 +1,28 @@
+import {SimpleEcr } from "@worldscapes/common";
+import {SimpleEngineServer, SimpleNetworkServer, SimpleServerSimulation, WebsocketServerNetworkAdapter } from "@worldscapes/server";
+
+async function init() {
+    console.log("Initializing Engine.");
+
+    console.log("Setting up network server.");
+    const serverAdapter = new WebsocketServerNetworkAdapter(50001);
+    await serverAdapter.isReady();
+    console.log("Network server is up.");
+
+    console.log("Creating simulation instance");
+    const ecr = new SimpleEcr();
+
+    console.log("Creating Engine Server.");
+    const server = new SimpleEngineServer(
+        new SimpleServerSimulation(ecr),
+        new SimpleNetworkServer(serverAdapter),
+    );
+
+
+    console.log("Starting Engine Server.");
+    server.start();
+
+    console.log("Initialized.");
+}
+
+init().then();
